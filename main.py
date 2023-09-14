c5eb2d0ff10612c2022dad2a7339eafd38a71aa7
autmku62122ucs19 <144676488+autmku62122ucs19@users.noreply.github.com>
2023-09-14 15:16:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+￼def fact_rec(n):
+ if n==0 or n==1:
+   return 1
+ 
+else:
+  return n*fact_rec(n_1)
+  
+number = 2
+res = fact_rec(number)
+
+print("The factorial of {} is {}". formet(number, rest)) 
